7a00d0dea560fad2e38c4656c325b5e78a48a6cb
jackomoffett <jack_moffett@icloud.com>
2021-02-21 18:04:35 +0000

    Made table headers more dynamic

--- pages/trades.js
+++ pages/trades.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
 export const Trades = () => {
+  const [headers, setHeaders] = useState([
+    "ticker",
+    "entry",
+    "quantity",
+    "exit",
+    "pl",
+  ]);
   const [trades, setTrades] = useState([
     {
       ticker: "amd",
@@ -28,17 +35,35 @@ export const Trades = () => {
     },
   ]);
 
+  const tableHeaders = () => {
+    return headers.map((header, index) => {
+      return (
+        <th
+          key={index}
+          className="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs"
+        >
+          {header}
+        </th>
+      );
+    });
+  };
+
   const tableRows = () => {
     return trades.map((trade, index) => {
       const pl = profitLoss(trade);
-      console.log(Math.sign(pl));
+
       return (
-        <tr key={index}>
-          <td>{trade.ticker}</td>
-          <td>{trade.entry}</td>
-          <td>{trade.quantity}</td>
-          <td>{trade.exit}</td>
-          <td className={pl > 0 ? "bg-green-200" : "bg-red-200"}>{pl}</td>
+        <tr key={index} className="text-left">
+          <td className="p-2 px-3">{trade.ticker.toUpperCase()}</td>
+          <td className="p-2 px-3">{trade.entry}</td>
+          <td className="p-2 px-3">{trade.quantity}</td>
+          <td className="p-2 px-3">{trade.exit}</td>
+          <td
+            className="p-2 px-3"
+            className={pl > 0 ? "bg-green-200" : "bg-red-200"}
+          >
+            {pl}
+          </td>
         </tr>
       );
     });
@@ -54,14 +79,10 @@ export const Trades = () => {
     // Account value at the start and current value
     //
     <>
-      <table className="table-auto">
+      <table className="table-auto rounded-lg whitespace-no-wrap bg-white table-striped relative">
         <thead>
-          <tr>
-            <th>Ticker</th>
-            <th>Entry</th>
-            <th>Quantity</th>
-            <th>Exit</th>
-            <th>P/L</th>
+          <tr className="text-left border-b border-gray-200 bg-gray-100">
+            {tableHeaders()}
           </tr>
         </thead>
         <tbody>{tableRows()}</tbody>
